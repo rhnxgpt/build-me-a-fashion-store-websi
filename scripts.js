75588002cad712326b4de2450823fb3ef645929b
Rohan Gupta <108945000+rhnxgpt@users.noreply.github.com>
2025-11-23 13:36:53 +0530

    Add scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+        // Add a simple scroll-to-top button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("scrollToTopBtn").style.display = "block";
+            } else {
+                document.getElementById("scrollToTopBtn").style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+    
